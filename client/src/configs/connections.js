75884b48bfaea9bd6d15a48a3877834af746ab19
Bartholomé <bartho.gili@gmail.com>
2022-01-28 11:49:29 +0100

    feat(config): prod connection urls

--- client/src/configs/connections.js
+++ client/src/configs/connections.js
@@ -1,11 +1,11 @@
 const baseURLConfig = {
     development: 'http://127.0.0.1:3000',
-    production: ''
+    production: 'http://127.0.0.1:3000'
 }
 
 const apiURLConfig = {
     development: 'https://cpoa.api.barthofu.com',
-    production: ''
+    production: 'https://cpoa.api.barthofu.com'
 }
 
 export const baseURL = baseURLConfig[process.env.NODE_ENV]
